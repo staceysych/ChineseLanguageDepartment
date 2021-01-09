cc4dd8170d83b02921b56e143c28e5beddb545d5
Stacey <stacey.sych@gmail.com>
2021-01-09 19:23:52 +0300

    feat: add Slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -3,19 +3,38 @@ import { Carousel } from 'antd';
 
 import './Slider.scss';
 
-import { mockedData, filterData } from '../../utils';
+import Button from '../Button';
 
-const Slider = () => {
-  const { teacherInfo } = filterData(mockedData, 'teachers');
-
+const Slider = ({ teacherInfo }) => {
   const contentStyle = {
-    height: '428px',
-    background: '#364d79',
+    height: '350px',
+    background: '#F6F0F0',
+    padding: '10px 20px',
+    display: 'flex',
+    justifyContent: 'space-between',
   };
 
   return (
     <div className="Slider">
-      <Carousel autoplay dotPosition="right">
+      <Carousel dotPosition="right" autoplay autoplaySpeed="50">
+        {teacherInfo.map((obj, index) => (
+          <div key={index}>
+            <div className="Slider__content" style={contentStyle}>
+              <div className="Slider__photo">
+                <img src={obj.photo} className="teachers-photo" />
+              </div>
+              <div className="Slider__description">
+                <div className="Slider__description_text">
+                  <h3>{obj.name}</h3>
+                  <p>{obj.info.position}</p>
+                  <p>{obj.info.degrees}</p>
+                  <p>{obj.info.subjects}</p>
+                </div>
+                <Button className="Slider__btn" text="Подробнее" />
+              </div>
+            </div>
+          </div>
+        ))}
       </Carousel>
     </div>
   );
--- src/components/TeachersPage/TeachersPage.jsx
+++ src/components/TeachersPage/TeachersPage.jsx
@@ -5,11 +5,20 @@ import './TeachersPage.scss';
 import Label from '../Label';
 import Slider from '../Slider';
 
+import { mockedData, filterData } from '../../utils';
+
 const TeachersPage = () => {
+  const {
+    label,
+    heading,
+    teacherInfo,
+  } = filterData(mockedData, 'teachers');
+
   return (
     <div className="TeachersPage container page">
-      <Label text="Преподаватели" />
-      <Slider />
+      <Label text={label} />
+      <h2 className="TeachersPage__title">{heading}</h2>
+      <Slider teacherInfo={teacherInfo} />
     </div>
   );
 };
