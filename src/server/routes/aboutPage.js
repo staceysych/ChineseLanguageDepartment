47121a89a5c506e5e8bbbac758a708952c638783
Stacey <stacey.sych@gmail.com>
2021-02-02 13:14:08 +0300

    Fix: responsive styles

--- src/server/routes/aboutPage.js
+++ src/server/routes/aboutPage.js
@@ -1,12 +1,11 @@
 const { Router } = require('express')
 const { Pages } = require('../models/page.model')
-const { toResponse } = require('../models/page.model');
 const router = Router()
 
 router.get('/', async (req, res) => {
     try {
         const page = await Pages.findOne({ page: 'about' });
-        res.status(200).json(toResponse(page));
+        res.status(200).json(page);
     } catch (e) {
         console.log(e.message);
     }
