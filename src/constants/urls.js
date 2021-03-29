2582b5401513308689b8ee9bf9de8d5ebeb19d1a
Stacey <stacey.sych@gmail.com>
2021-03-29 17:24:47 +0300

    localHost server

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,8 +1,10 @@
 export default {
-    SERVER_URL: 'https://chinese-department-mslu.herokuapp.com/',
+    SERVER_URL: 'http://localhost:4000/',
     UPLOAD : 'file/upload',
     DELETE: 'file/delete/',
     UPLOAD_FILE: 'file/upload/file',
     MULTIPLE_UPLOAD_FILE: 'file/multiple-file-upload',
     LOGIN: 'auth/login',
 };
+
+/* 'https://chinese-department-mslu.herokuapp.com/', */
