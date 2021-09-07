d312105bceb46d8909d203d3b8d62e091378a679
Ymbi 19 <tropaul322@gmail.com>
2021-09-07 19:50:02 +0300

    fix: change url consts

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,7 +1,7 @@
 export default {
     SERVER_URL: 'https://chinese-department-server.herokuapp.com/',
     UPLOAD : 'file/upload',
-    DELETE: 'file/delete/file',
+    DELETE: 'file/delete/file/',
     UPLOAD_FILE: 'file/upload/file',
     MULTIPLE_UPLOAD_FILE: 'file/multiple-file-upload',
     LOGIN: 'auth/login',
